85a099bd54a0d03b47a69f6ec1157a47231b73bc
Юрий <fedejkin@gmail.com>
2024-07-31 10:26:59 +0300

    Inicial commit

---
+++ src/main/java/pro/sky/Spring_2_11/Spring211Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Spring_2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring211Application.class, args);
+	}
+
+}
